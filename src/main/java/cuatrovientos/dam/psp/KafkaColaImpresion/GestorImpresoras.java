158365b683a9ebf6f12033f9f6c1def2101847e8
Daniel <tretoleondaniel@gmail.com>
2026-01-31 00:52:00 +0100

    feat: crear lanzador GestorImpresoras para inicializar y gestionar el ciclo de vida de múltiples hilos de impresoras

--- src/main/java/cuatrovientos/dam/psp/KafkaColaImpresion/GestorImpresoras.java
+++ src/main/java/cuatrovientos/dam/psp/KafkaColaImpresion/GestorImpresoras.java
@@ -1,8 +1,24 @@
 package cuatrovientos.dam.psp.KafkaColaImpresion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorImpresoras {
 
     public static void main(String[] args) {
+        System.out.println("===== GESTOR DE IMPRESORAS INICIADO =====");
         
+        List<Thread> impresoras = new ArrayList<>();
+
+        impresoras.add(new Thread(new Impresora("BN", "BN_Oficina1")));
+        impresoras.add(new Thread(new Impresora("BN", "BN_Pasillo")));
+        impresoras.add(new Thread(new Impresora("BN", "BN_RecursosHumanos")));
+        
+        impresoras.add(new Thread(new Impresora("Color", "Color_Marketing")));
+        impresoras.add(new Thread(new Impresora("Color", "Color_Direccion")));
+
+        for (Thread t : impresoras) {
+            t.start();
+        }
     }
 }
