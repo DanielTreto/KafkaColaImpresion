4cbec754dc860e7ce816bd29a3b02737dac530ce
Daniel <tretoleondaniel@gmail.com>
2026-01-30 22:56:00 +0100

    feat: implementar modelo de datos TrabajoImpresion para encapsular información del documento con serialización JSON

--- src/main/java/cuatrovientos/dam/psp/KafkaColaImpresion/TrabajoImpresion.java
+++ src/main/java/cuatrovientos/dam/psp/KafkaColaImpresion/TrabajoImpresion.java
@@ -1,5 +1,30 @@
 package cuatrovientos.dam.psp.KafkaColaImpresion;
 
-public class TrabajoImpresion {
+import com.google.gson.Gson;
 
+public class TrabajoImpresion {
+    private String titulo;
+    private String documento;
+    private String tipo; 
+    private String sender; 
+
+    public TrabajoImpresion(String titulo, String documento, String tipo, String sender) {
+        this.titulo = titulo;
+        this.documento = documento;
+        this.tipo = tipo;
+        this.sender = sender;
+    }
+
+    public String getTitulo() { return titulo; }
+    public String getDocumento() { return documento; }
+    public String getTipo() { return tipo; }
+    public String getSender() { return sender; }
+    
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
+
+    public static TrabajoImpresion fromJson(String json) {
+        return new Gson().fromJson(json, TrabajoImpresion.class);
+    }
 }
